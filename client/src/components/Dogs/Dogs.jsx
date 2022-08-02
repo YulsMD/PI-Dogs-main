7d161899fe8e8ba3b21606b20631648c4cf95cc4
Yulissa Martínez <yulissa.mtzd@gmail.com>
2022-08-02 18:43:53 -0500

    Functional Component to Class Component

--- client/src/components/Dogs/Dogs.jsx
+++ client/src/components/Dogs/Dogs.jsx
@@ -1,13 +1,66 @@
-import {React, useEffect, useState} from "react";
-import { useDispatch} from "react-redux";
-import {getAllDogs, getAllTemperaments } from "../../redux/actions";
-import DogCard from '../DogCard/DogCard';
+import { React, Component, useState } from "react";
+import { connect, useDispatch } from "react-redux";
+import { getAllDogs, getAllTemperaments } from "../../redux/actions";
+import DogCard from "../DogCard/DogCard";
 import Loader from "../Loader/Loader";
-import s from '../Dogs/Dogs.module.css'
-import NotFound from "../NotFound/NotFound";
+import s from "../Dogs/Dogs.module.css";
 
+export class Dogs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loader: false,
+      currentDogs: this.props.currentDogs,
+    };
+  }
 
-export default function Dogs({currentDogs}){
+  componentDidMount() {
+    this.setState({
+      loader: true,
+    });
+    this.props.getDogs();
+    this.props.getTemperaments();
+    this.setState({ loader: false });
+  }
+  render() {
+    console.log(this.props.currentDogs);
+    return (
+      <div>
+        {this.state.loader ? (
+          <div className={s.loader}>
+            <Loader />
+          </div>
+        ) : (
+          <div className={s.container}>
+            {this.props.currentDogs?.map((dog) => {
+              return (
+                <DogCard
+                  key={dog.id}
+                  id={dog.id}
+                  name={dog.name}
+                  temperaments={dog.temperaments}
+                  weight={dog.weight}
+                  image={dog.image}
+                />
+              );
+            })}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    getDogs: () => dispatch(getAllDogs()),
+    getTemperaments: () => dispatch(getAllTemperaments()),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Dogs);
+
+/* export default function Dogs({currentDogs}){
   
   const [loader, setLoader] = useState(false)
 
@@ -42,4 +95,4 @@ export default function Dogs({currentDogs}){
     }
     </div>
   )
-}
+} */
